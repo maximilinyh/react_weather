9daf360072e3b81f5616590595c900464efd76bf
Maxim Ilinyh <maxim_ilinyh@mail.ru>
2020-05-04 15:36:42 +0700

    optimizatiom geo 04/05

--- src/Components/CityGeo.js
+++ src/Components/CityGeo.js
@@ -6,20 +6,35 @@ import {useDispatch} from "react-redux";
 function handlerGetLocation(dispatch) {
         navigator.geolocation.getCurrentPosition(
         function(position) {
-            let latValue = position.coords.latitude.toFixed(),
-                longValue = position.coords.longitude.toFixed()
-            const result = city_list.filter((item)=> {
-                let latQuery = item.coord.lat.toFixed(),
-                    longQuery = item.coord.lon.toFixed()
 
-                return latValue === latQuery && longValue === longQuery
+            //current coords
+            let latValue = position.coords.latitude,
+                longValue = position.coords.longitude
+
+            //get data cities coords
+            const citiesCoords = [...city_list].map((city)=> {
+                return {
+                    id: city.id,
+                    lat: city.coord.lat,
+                    lon: city.coord.lon,
+                }
             })
 
-            const id = result.map((item)=> {
-                return item.id
-            })
+            //filter coords
+            let result = latValue < 0
+                ?
+                citiesCoords.filter(cur => cur.lat < latValue && cur.lon < longValue).sort((a, b) => {
+                    return a.lat > b.lat && a.lon > b.lon? 1 : -1
+                })[0]
+                :
+                citiesCoords
+                    .filter(cur => cur.lat > latValue && cur.lon > longValue)
+                    .sort((a, b) => {
+                        return a.lat > b.lat && a.lon > b.lon? 1 : -1
+                })[0];
 
-            dispatch(fetchCitySuccess(dispatch, id[0]))
+            //dispatch action
+            dispatch(fetchCitySuccess(dispatch, result.id))
 
         },
 
